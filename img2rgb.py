a1c84b5312fcbb966a6fdfa4775367b33db7c97d
Nikolaus Georgi <nxg@student.gunadarma.ac.id>
2023-10-18 00:52:11 +0700

    add histogram feature

--- img2rgb.py
+++ img2rgb.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from pandas import DataFrame
 from PIL import Image
 import streamlit as st
@@ -35,6 +36,73 @@ if uploaded_image is not None:
         df.columns += 1
         df.index.name = "Px"
 
-    with st.spinner("Displaying result..."):
+    with st.spinner("Displaying pixel data..."):
+        st.subheader("Pixel Data")
         st.dataframe(df)
         st.caption("Note: Each cell represents the RGB value of a pixel in the image.")
+
+    st.divider()
+
+    n_channels = 4
+    n_colors = 256
+
+    with st.spinner("Calculating pixel frequency..."):
+        px_freq = [[0 for _ in range(n_colors)] for _ in range(n_channels)]
+        for y in pixel_rgbs:
+            for rgb in y:
+                for c in range(3):
+                    px_freq[c][rgb[c]] += 1
+                grayscale = round(sum(rgb) / 3)
+                px_freq[3][grayscale] += 1
+
+    with st.spinner("Displaying pixel frequency data..."):
+        df = DataFrame(px_freq, index=["R", "G", "B", "GS"])
+        st.subheader("Pixel Frequency Data")
+        st.dataframe(df)
+
+    labels = ["Red", "Green", "Blue", "Gray"]
+
+    with st.spinner("Creating RGB and Grayscale histograms..."):
+        fig, ax = plt.subplots(2, 2, figsize=(10, 8))
+        for c in range(n_channels):
+            row, col = divmod(c, 2)
+            ax[row, col].bar(
+                range(n_colors), px_freq[c], label=labels[c], color=labels[c]
+            )
+            ax[row, col].set_title(labels[c])
+            ax[row, col].set_xlabel("Pixel Intensity")
+            ax[row, col].set_ylabel("Pixel Frequency")
+        plt.suptitle("RGB Grayscale Histograms")
+        plt.tight_layout()
+
+        st.subheader("RGB Grayscale Histograms")
+        st.pyplot(fig)
+
+    st.divider()
+
+    with st.spinner("Normalizing histograms..."):
+        total_pixels = image.width * image.height
+        for i in range(n_channels):
+            for j in range(n_colors):
+                px_freq[i][j] /= total_pixels
+
+    with st.spinner("Displaying normalized pixel frequency data..."):
+        df = DataFrame(px_freq, index=["R", "G", "B", "GS"])
+        st.subheader("Normalized Pixel Frequency Data")
+        st.dataframe(df)
+
+    with st.spinner("Creating normalized RGB and Grayscale histograms..."):
+        fig, ax = plt.subplots(2, 2, figsize=(10, 8))
+        for c in range(n_channels):
+            row, col = divmod(c, 2)
+            ax[row, col].bar(
+                range(n_colors), px_freq[c], label=labels[c], color=labels[c]
+            )
+            ax[row, col].set_title(labels[c])
+            ax[row, col].set_xlabel("Pixel Intensity")
+            ax[row, col].set_ylabel("Pixel Frequency")
+        plt.suptitle("Normalized Histograms")
+        plt.tight_layout()
+
+        st.subheader("Normalized Histograms")
+        st.pyplot(fig)
